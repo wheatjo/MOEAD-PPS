d7be736fbc6b690a49e41bdc5e2d5e5664c12643
wheat <ruitaomai@163.com>
2022-04-23 14:05:19 +0800

    the first complete finsh

--- test/individual_init_test.py
+++
@@ -1,9 +0,0 @@
-from deap import base
-from deap import tools
-from deap import creator
-import numpy as np
-from functools import partial
-from pymoo.factory import get_problem
-from pymoo.factory import get_mutation
-from pymoo.interface import mutation
-
